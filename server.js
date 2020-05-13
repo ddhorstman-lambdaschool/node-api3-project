4509a7ce2a6e884212cbd10a8c82ed57dbad8ac4
David Horstman <davidzilla27@gmail.com>
2020-05-13 00:54:32 -0400

    Fix endpoint URI

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const server = express();
 server.use(express.json());
 server.use(logger);
 
-server.use("/users", userRouter);
-server.use("/posts", postRouter);
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
