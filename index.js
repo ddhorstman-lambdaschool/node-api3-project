dfb0b4b40a82a342dfed9b39ad76a6a30674c44b
David Horstman <davidzilla27@gmail.com>
2020-05-13 23:28:18 -0400

    Set up for Heroku

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const server = require("./server");
 
-const PORT = 5000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+const LOCATION = process.env.LOCATION || "unknown";
 
 server.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Server hosted on ${LOCATION}`);
+  console.log(`Listening on port ${PORT}`);
 });
